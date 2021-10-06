fa9e994199b4d1eb80c22968e90d053808428e1e
gabynina <79166259+gabynina@users.noreply.github.com>
2021-10-06 18:38:35 -0400

    lab 5

---
+++ Labs/Lab 5/Submission/script.js
@@ -0,0 +1,10 @@
+// work outside in: call body, then section, then class. 
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByClassName('title')[0].innerHTML= 'Change';
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('p')[0].innerHTML= 'Gabriella Nina';
+document.getElementById('bossy').innerHTML='Please do the following';
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('ul')[0].getElementsByTagName('li')[0].style.color="blue";
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('ul')[0].getElementsByTagName('li')[1].style.backgroundColor='beige';
+document.body.style.backgroundColor = "gray";
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('ul')[0].getElementsByTagName('li')[3].style.textTransform = 'uppercase';
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('ul')[0].getElementsByTagName('li')[4].setAttribute('class', 'title');
+    
